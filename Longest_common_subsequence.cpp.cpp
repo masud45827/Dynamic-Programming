ad047b569d1ba9dc6c3041d1a4bc10c43e562c8f
masud45827 <60808203+masud45827@users.noreply.github.com>
2023-02-22 11:23:40 +0600

    Update Longest_common_subsequence.cpp.cpp

--- Longest_common_subsequence.cpp.cpp
+++ Longest_common_subsequence.cpp.cpp
@@ -1,34 +1,6 @@
 #include<bits/stdc++.h>
-#include <iostream>
-#include <algorithm>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <string>
-#include <cmath>
-#include <vector>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <iterator>
-#include <bitset>
-#include <assert.h>
-#include <new>
-#include <sstream>
-#include <time.h>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <limits>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
 typedef     long long    ll;
-typedef     unsigned long long    ull;
 typedef     vector<int> vi;
 typedef     vector<long long> vl;
 typedef     pair<int, int>pi;
@@ -37,68 +9,72 @@ typedef     pair<long long, long long>pl;
 #define S   second
 #define pb  push_back
 #define     all(x)      x.begin() , x.end()
-#define       FOR(i,a,b) for(i=a;i<=b;i++)
+#define mp  make_pair
+#define     REP(i,a,b) for(i=a;i<=b;i++)
 #define     mem(a)      memset(a , 0 ,sizeof a)
 #define     memn(a)     memset(a , -1 ,sizeof a)
-#define setpr(x) cout<<setprecision(x)<<fixed
-#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define endl '\n'
-template <typename T> inline void Int(T &a) {
-    bool minus = false; a = 0; char ch = getchar();
-    while (true) { if (ch == '-' or (ch >= '0' && ch <= '9')) break; ch = getchar(); }
-    if (ch == '-') minus = true; else a = ch - '0';
-    while (true) { ch = getchar(); if (ch < '0' || ch > '9') break; a = a * 10 + (ch - '0'); }
-    if (minus)a *= -1 ;
-}
-#ifdef LOCAL
-#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
-void err(istream_iterator<string> it) {cout << endl ;}
-template<typename T, typename... Args>
-void err(istream_iterator<string> it, T a, Args... args) {
-    cerr << *it << " = " << a << ' ' ;
-    err(++it, args...);
-}
-#else
-#define error(args...)
-#endif
-
-const int N          = (int)2e5 + 5 ;
-const int maxN       = (int)1e6 + 6 ;
-const ll  Mod        = (ll)1e9 + 7 ;
-const int inf        = (int)2e9 ;
-const ll  Inf        = (ll)1e18 ;
-const int mod        = (int)1e9 + 7 ;
-const double EPS     = (double)1e-9 ;
-const double PI      = (double)acos(-1.0) ;
-
-
-inline int add(int a, int b, int mod) {a += b ; return a >= mod ? a - mod : a ;}
-inline int sub(int a, int b, int mod) {a -= b ; return a < 0 ? a + mod : a ;}
-inline int mul(int a, int b, int mod) {return (ll)a * b % mod ;}
-ll a[N + 3];
-ll dp[N + 3][2], n;
-ll fun(ll i,ll prev) {
-    if (i > n) return 0;
-    if (dp[i][prev] != -1) return dp[i][prev];
-    if (prev == 0) {
-        ll x = a[i] + fun(i+1,1);
-        ll y = fun(i + 1,0);
-        return dp[i][prev] = max(x, y);
-    }else{
-         ll x=fun(i+1,0);
-         return dp[i][prev]=x;
+// int lcs(string s1, string s2, int x, int y)
+// {
+//   if (x == 0 || y == 0) return 0;
+//   if (s1[x - 1] == s2[y - 1])
+//   {
+//     return 1 + fun(s1, s2, x - 1, y - 1);
+//   } else
+//   {
+//     return max(fun(s1, s2, x - 1, y), fun(s1, s2, x, y - 1));
+//   }
+// }
+// string lcs_string_ptint(string s1,string s2,int n,int m){
+//     int dp[n+2][m+2],i,j;
+//     for(i=0;i<=n;i++){
+//         for(j=0;j<=m;j++){
+//              if(i==0||j==0) dp[i][j]=0;
+//              else if(s1[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+//              else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+//         }
+//     }
+//      int mx=dp[n][m];
+//       i=n; j=m;
+//       string s;
+//        while(i>0&&j>0){
+//          if(s1[i-1]==s2[j-1]) {
+//             s.push_back(s1[i-1]);
+//             i--;j--;
+//         }else{
+//              if(dp[i-1][j]>dp[i][j-1]) i--;
+//              else j--;
+//         }
+//        }
+//        reverse(s.begin(),s.end());
+//        return s;
+//  }
+int lcs(string  X, string Y, int m, int n)
+{
+    int L[m + 1][n + 1];
+    int i, j;
+    for (i = 0; i <= m; i++) {
+        for (j = 0; j <= n; j++) {
+            if (i == 0 || j == 0)
+                L[i][j] = 0;
+  
+            else if (X[i - 1] == Y[j - 1])
+                L[i][j] = L[i - 1][j - 1] + 1;
+  
+            else
+                L[i][j] = max(L[i - 1][j], L[i][j - 1]);
+        }
     }
+    return L[m][n];
 }
-int main() {
-    int test = 1, fac = 1;
-    //cin>>test;
-    while (test--) {
-        ll i, j, x, y;
-        cin >> n;
-        memn(dp);
-        for (i = 1; i <= n; i++) cin >> a[i];
-        cout << fun(1,0) << endl;
-    }
-    return 0;
+int main()
+{
+  int t = 1;
+// cin >> t;
+  while (t--)
+  {
+    string s1, s2;
+    cin >> s1 >> s2;
+    int x = lcs(s1, s2, s1.size(), s2.size());
+    cout << x << endl;
+  }
 }
-
