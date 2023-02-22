adc409fe081481280f6f812b9eacd722288e41ab
masud45827 <60808203+masud45827@users.noreply.github.com>
2023-02-22 11:22:25 +0600

    Update Longest_common_subsequence.cpp.cpp

--- Longest_common_subsequence.cpp.cpp
+++ Longest_common_subsequence.cpp.cpp
@@ -1,6 +1,34 @@
 #include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <iterator>
+#include <bitset>
+#include <assert.h>
+#include <new>
+#include <sstream>
+#include <time.h>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
 typedef     long long    ll;
+typedef     unsigned long long    ull;
 typedef     vector<int> vi;
 typedef     vector<long long> vl;
 typedef     pair<int, int>pi;
@@ -9,30 +37,68 @@ typedef     pair<long long, long long>pl;
 #define S   second
 #define pb  push_back
 #define     all(x)      x.begin() , x.end()
-#define mp  make_pair
-#define     REP(i,a,b) for(i=a;i<=b;i++)
+#define       FOR(i,a,b) for(i=a;i<=b;i++)
 #define     mem(a)      memset(a , 0 ,sizeof a)
 #define     memn(a)     memset(a , -1 ,sizeof a)
-int fun(string s1, string s2, int x, int y)
-{
-  if (x == 0 || y == 0) return 0;
-  if (s1[x - 1] == s2[y - 1])
-  {
-    return 1 + fun(s1, s2, x - 1, y - 1);
-  } else
-  {
-    return max(fun(s1, s2, x - 1, y), fun(s1, s2, x, y - 1));
-  }
+#define setpr(x) cout<<setprecision(x)<<fixed
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define endl '\n'
+template <typename T> inline void Int(T &a) {
+    bool minus = false; a = 0; char ch = getchar();
+    while (true) { if (ch == '-' or (ch >= '0' && ch <= '9')) break; ch = getchar(); }
+    if (ch == '-') minus = true; else a = ch - '0';
+    while (true) { ch = getchar(); if (ch < '0' || ch > '9') break; a = a * 10 + (ch - '0'); }
+    if (minus)a *= -1 ;
 }
-int main()
-{
-  int t = 1;
-// cin >> t;
-  while (t--)
-  {
-    string s1, s2;
-    cin >> s1 >> s2;
-    int x = fun(s1, s2, s1.size(), s2.size());
-    cout << x << endl;
-  }
+#ifdef LOCAL
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {cout << endl ;}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+    cerr << *it << " = " << a << ' ' ;
+    err(++it, args...);
 }
+#else
+#define error(args...)
+#endif
+
+const int N          = (int)2e5 + 5 ;
+const int maxN       = (int)1e6 + 6 ;
+const ll  Mod        = (ll)1e9 + 7 ;
+const int inf        = (int)2e9 ;
+const ll  Inf        = (ll)1e18 ;
+const int mod        = (int)1e9 + 7 ;
+const double EPS     = (double)1e-9 ;
+const double PI      = (double)acos(-1.0) ;
+
+
+inline int add(int a, int b, int mod) {a += b ; return a >= mod ? a - mod : a ;}
+inline int sub(int a, int b, int mod) {a -= b ; return a < 0 ? a + mod : a ;}
+inline int mul(int a, int b, int mod) {return (ll)a * b % mod ;}
+ll a[N + 3];
+ll dp[N + 3][2], n;
+ll fun(ll i,ll prev) {
+    if (i > n) return 0;
+    if (dp[i][prev] != -1) return dp[i][prev];
+    if (prev == 0) {
+        ll x = a[i] + fun(i+1,1);
+        ll y = fun(i + 1,0);
+        return dp[i][prev] = max(x, y);
+    }else{
+         ll x=fun(i+1,0);
+         return dp[i][prev]=x;
+    }
+}
+int main() {
+    int test = 1, fac = 1;
+    //cin>>test;
+    while (test--) {
+        ll i, j, x, y;
+        cin >> n;
+        memn(dp);
+        for (i = 1; i <= n; i++) cin >> a[i];
+        cout << fun(1,0) << endl;
+    }
+    return 0;
+}
+
